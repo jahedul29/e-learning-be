4ff70d58c45f3151f26c4154866cc245ee118849
Jahedul Hoque <jahedulhoque@Jaheduls-MacBook-Pro.local>
2024-12-18 22:10:51 +0600

    initial commit

---
+++ config/backend-domain.js
@@ -0,0 +1,6 @@
+// Production mode
+const BACKEND_URL = process.env.BACKEND_URL;
+
+// const BACKEND_URL = "http://localhost:9000"; // Development mode!!!
+
+exports.BACKEND_URL = BACKEND_URL;
---
+++ config/constant.js
@@ -0,0 +1,9 @@
+const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
+// const YOUTUBE_API_KEY = "AIzaSyDCh4pDvYFVJe5Lu_DbHT_kBgrJma83FxI";
+// const YOUTUBE_API_KEY = "AIzaSyCjjqjn7wqsKm5EcKn-n3gMJgcl3-iOlpc";
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+// const OPEN_AI_KEY = "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";
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
+exports.UNSPLASH_API_KEY = UNSPLASH_API_KEY;
+exports.YOUTUBE_API_KEY = YOUTUBE_API_KEY;
+exports.OPEN_AI_KEY = OPEN_AI_KEY;
